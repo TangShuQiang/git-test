e4c0828511fa3b3de88895a93f9b56899081331c
tangshuqiang <tangshuqiang@qq.com>
2022-03-20 20:10:30 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author tang_shu_qiang
+ * @create 2022-03-20 20:01
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
